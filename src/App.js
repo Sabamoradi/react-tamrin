680b827db1a2020f3d1138b2a19fea73c221edb7
sabamoradi <saba.moradi7719@gmail.com>
2023-05-12 19:07:49 +0430

    add new questions

--- src/App.js
+++ src/App.js
@@ -27,3 +27,5 @@ export default App;
 // how we can bind more than one class for item
 //why we should use use reducer when we have redux
 //why we should have ... in set item in tabs
+//advantage of have asyncronous data in action of state management
+//find out what is the route of our component
